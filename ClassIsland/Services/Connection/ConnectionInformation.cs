cada75b248f9bdafb737e48e717186dcfaea3cda
爱玩电脑的WindowsXP <132193112+AWDNDWindowsXP@users.noreply.github.com>
2025-02-03 16:07:00 +0800

    feat:连接信息

---
+++ ClassIsland/Services/Connection/ConnectionInformation.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ClassIsland.Services.Connection
+{
+    public class ConnectionInfo
+    {
+        public string IpAddress { get; set; } = Dns.GetHostAddresses(Dns.GetHostName()).ToString();
+    }
+}
